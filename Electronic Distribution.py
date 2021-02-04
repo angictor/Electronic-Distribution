504685a77002236ebf21f0ea4746465ef7997cec
angictor <51858597+angictor@users.noreply.github.com>
2021-02-04 11:19:17 +0100

    Add files via upload

---
+++ Electronic Distribution.py
@@ -0,0 +1,62 @@
+print('K***************A***********************D******************I')
+print('*****      Welcome to the Electronic Distribution      *****')
+print('*****                    Program                       *****')
+print('********R*******E******C***************H*****E**************')
+print('    *  WARNING')
+print('   * *  YOU CANNOT')
+print('  *   *  ENTER THE VALUE')
+print(' *******  ONE *ATOM*  :')
+print(' ')
+# #/////////////////////
+atom = input("Pleas Enter the Name of the Atom : ")
+value = int(input("Pleas Enter the Value of the Atom : "))
+x = value + 0
+k = 0
+l = 0
+m = 0
+value1 =value
+# /////////////////////
+if value >= 2:
+    value = value - 2
+    k = 2
+else:
+    k = 1
+    value = 0  
+#   //////////////////////
+if value >= 8:
+    value = value - 8
+    l = 8
+else:
+    l = value
+    value = 0
+#/////////////////////////
+if value >= 16:
+    value = value -16
+    m = 16
+else:
+    m = value
+#/////////////////////////
+val1 = str(atom)
+val3 = str(k)
+val4 = str(l)
+val5 = str(m)
+val6 = str(x)
+#/////////////////////////
+print(" ")
+print(" ")
+print("'---------------------------------------'")
+print("' Name Atom  ==>  " + val6 + " " + val1 )
+print("'---------------------------------------'")
+print("' Electronic Distribution ==>  " "K" +val3+ " L" +val4+ " M" +val5)
+print("'---------------------------------------'")
+print(" ")
+print(" ")
+print("*****************************************  KADI,RECHE :/")
+print(" ")
+print(" ==============> PROGRAM MAKER  :   KADI RECHE  :  PROGRAM MAKER <==============")
+print("")
+print("Link Facebook ==> https://www.facebook.com/kadi.reche.31")
+print("")
+print("Link GitHub ==> https://github.com/angictor")
+print(" ")
+print(" ===============================================================================")
